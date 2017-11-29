b5c2dd56dd4e18cdfce7c87dd1cff8e2540afdf5
crushmyr <crushmyr@gmail.com>
2017-11-29 21:35:55 +0800

    实现了单线程下载

--- mm_crawler.py
+++ mm_crawler.py
@@ -2,8 +2,41 @@
 # -*- coding: utf-8 -*-
 
 
+import re
+import os
+import requests
+import urllib
 from docopt import docopt
-from pprint import pprint
+
+
+def scan_url(url, url_to_scan_patt, img_to_down_patt):
+    url_to_scan_lst = []
+    img_to_down_lst = []
+    try:
+        req = requests.get(url)
+        if req.status_code == 200:
+            url_to_scan_lst = re.findall(url_to_scan_patt, req.text)
+            url_to_scan_lst = [urllib.parse.urljoin(url, i)
+                               for i in url_to_scan_lst]
+            img_to_down_lst = re.findall(img_to_down_patt, req.text)
+    except Exception as e:
+        pass
+    return (list(set(url_to_scan_lst)), list(set(img_to_down_lst)))
+
+
+def down_img(url, path):
+    if not os.path.isdir(path):
+        os.mkdir(path)
+    filename = url.split('/')[-1]
+    fullname = os.path.join(path, filename)
+    print('down_picture from url:{0}'.format(url))
+    try:
+        req = requests.get(url)
+        if req.status_code == 200:
+            with open(fullname, 'wb') as f:
+                f.write(req.content)
+    except Exception as e:
+        print(e)
 
 
 def main():
@@ -24,7 +57,54 @@ Example:
     python mm_crawler.py -n 20 -o ~/Downloads -l 100
 """
     arguments = docopt(doc)
-    pprint(arguments)
+    path = arguments['-o']
+    if not os.path.isdir(path):
+        try:
+            os.mkdir(path)
+        except Exception as e:
+            print('请输入正确的目录路径')
+
+    try:
+        limit = int(arguments['-l'])
+    except Exception as e:
+        print('-l 后面的参数必须是数字')
+
+    start_url = 'http://www.juemei.com/mm/'
+    url_to_scan_patt = r'/mm/[^"\']+'
+    img_to_down_patt = (r'http://img\.juemei\.com/\w+/'
+                        r'\d{4}-\d{2}-\d{2}/\w{13}\.jpg')
+
+    url_to_scan_set = set(start_url)
+    url_to_scan_set = set()
+    img_to_down_set = set()
+    url_to_scan_lst_temp, img_to_down_lst_temp = scan_url(start_url,
+                                                          url_to_scan_patt,
+                                                          img_to_down_patt)
+    have_new_url_to_scan = True
+    while have_new_url_to_scan:
+        have_new_url_to_scan = False
+        url_to_scan_lst = url_to_scan_lst_temp
+        img_to_down_lst = img_to_down_lst_temp
+        url_to_scan_lst_temp = []
+        img_to_down_lst_temp = []
+
+        for url_item in url_to_scan_lst:
+            if url_item not in url_to_scan_set:
+                have_new_url_to_scan = True
+                url_to_scan_set.add(url_item)
+                a, b = scan_url(url_item, url_to_scan_patt, img_to_down_patt)
+                for i in a:
+                    url_to_scan_lst_temp.append(i)
+                for i in b:
+                    img_to_down_lst_temp.append(i)
+
+        for img_item in img_to_down_lst:
+            if img_item not in img_to_down_set:
+                down_img(img_item, path)
+                img_to_down_set.add(img_item)
+
+        if len(img_to_down_set) >= limit:
+            break
 
 
 if __name__ == '__main__':
